54b60c4651ec9c2b78a55556cabe8e44eb4bea03
William Gallo <williamgalloh@gmail.com>
2022-10-18 15:08:58 -0500

    Added 'show per page' functionality and result details.
    Added the ability to select how many items to show per page. Also added some text to show some details about the results being displayed.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,22 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-// Initialize constant that will determine how many students to show per page
-const items_per_page = 9;
+// Initialize variable that will determine how many students to show per page
+let items_per_page = 9;
+
+// Initialize variable that will hold current student data, this will help when filtering and modifying the items_per_page setting
+let current_list = data;
+
+// Initialize variable that will keep track of the page we are currently on
+let current_page = 1;
+
+function getStartIndex() {
+   return (current_page * items_per_page) - items_per_page;
+}
+
+function getEndIndex() {
+   return current_page * items_per_page;
+}
 
 /**
  * showPage
@@ -15,8 +29,8 @@ const items_per_page = 9;
  * @param {integer} page - Page index to show 
  */
 function showPage(list, page) {
-   let start_index = (page * items_per_page) - items_per_page;
-   let end_index = page * items_per_page;
+   let start_index = getStartIndex();
+   let end_index = getEndIndex();
 
    let student_list = document.querySelector(".student-list");
 
@@ -39,7 +53,6 @@ function showPage(list, page) {
 
    student_list.innerHTML = list_items;
 }
-
 
 /**
  * addPagination
@@ -65,9 +78,30 @@ function addPagination(list) {
    if(total_pages > 0) {
       // Highlight first pagination link
       document.querySelector('.link-list li:first-child button').classList.add("active");
+
+      addResultDetails();
    }
 }
 
+/**
+ * addResultDetails
+ * 
+ * Adds a summary of the results displayed on the page
+ */
+function addResultDetails() {
+   let start_index = getStartIndex();
+   let end_index = getEndIndex();
+   let total_entries = current_list.length;
+
+   // Calculate real end index is last page contains less items than items_per_page value
+   if(end_index > total_entries) {
+      let last_page_count = total_entries % items_per_page;
+      end_index = (start_index + last_page_count);
+   }
+
+   let details = `<p class="result-details">Showing ${start_index + 1} to ${end_index} of ${total_entries} entries<p>`;
+   document.querySelector('.result-details').innerHTML = details;
+}
 
 /**
  * addSearchbar
@@ -81,26 +115,37 @@ function addSearchbar() {
       <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
     </label>`;
 
-    let header = document.querySelector('header');
-    header.insertAdjacentHTML('beforeend', search_bar);
+   let header = document.querySelector('header');
+   header.insertAdjacentHTML('beforeend', search_bar);
 }
 
-// Call functions
-showPage(data, 1);
-addPagination(data);
-addSearchbar();
+/**
+ * addPerPageDropdown
+ * 
+ * Adds dropdown element to the page which changes the number of items displayed per page
+ */
+function addPerPageDropdown() {
+   const dropdown = `<div class="students-per-page">
+      <label>Show per page:</label>
+      <select id="items-per-page">
+         <option value="9" selected="selected">9</option>
+         <option value="18">18</option>
+         <option value="27">27</option>
+         <option value="36">36</option>
+      </select>
+   </div>`;
 
-// Handle page navigation
-document.querySelector('.link-list').addEventListener('click', (e) => {
-   if(e.target.tagName === "BUTTON") {
-      let button = e.target;
-      let new_page_index = button.dataset.page;
-      showPage(data, new_page_index);
-      document.querySelector('.link-list button.active').classList.remove("active");
-      button.classList.add('active');
-   }
-});
+   let header = document.querySelector('header');
+   header.insertAdjacentHTML('beforeend', dropdown);
+}
 
+/**
+ * search
+ * 
+ * Handles search functionality
+ *
+ * @param {Object Event} e - Event object passed by event listener
+ */
 let search = (e) => {
    let search_query = document.querySelector('.student-search input').value.toLowerCase();
 
@@ -114,8 +159,10 @@ let search = (e) => {
       }
    }
 
+   current_list = new_list;
+
    // CAN BE IMPROVED ****
-   if(new_list.length > 0) {
+   if(current_list.length > 0) {
       if(document.querySelector('.no-results')) {
          document.querySelector('.no-results').remove();
       }
@@ -123,10 +170,37 @@ let search = (e) => {
       document.querySelector('header').insertAdjacentHTML('afterend', '<h3 class="no-results">No results found</h3>');
    }
 
-   showPage(new_list, 1);
-   addPagination(new_list);
+   showPage(current_list, 1);
+   addPagination(current_list);
 };
 
-// Handle search functionality
+// Call functions
+showPage(current_list, 1);
+addPagination(current_list);
+addPerPageDropdown();
+addSearchbar();
+
+// Handle page navigation
+document.querySelector('.link-list').addEventListener('click', (e) => {
+   if(e.target.tagName === "BUTTON") {
+      let button = e.target;
+      let new_page_index = button.dataset.page;
+      current_page = new_page_index;
+      showPage(current_list, new_page_index);
+      addResultDetails();
+      document.querySelector('.link-list button.active').classList.remove("active");
+      button.classList.add('active');
+   }
+});
+
 document.querySelector('.student-search input').addEventListener('keyup', search);
 document.querySelector('.student-search button').addEventListener('click', search);
+
+// Handle items per page functionality
+document.querySelector('#items-per-page').addEventListener('change', (e) => {
+   let dropdown = e.target;
+   items_per_page = dropdown.value;
+   current_page = 1;
+   showPage(current_list, 1);
+   addPagination(current_list);
+});
